e8486943322d25be626584712a1efdbdcf244f64
Abdulqohar <sabdulqohar9@gmail.com>
2021-10-24 22:16:02 +0100

    fixed duplicate string and dimens resource bug

--- app/src/main/java/com/abdulqohar/hagglex/ui/fragments/VerifyAccountFragment.kt
+++ app/src/main/java/com/abdulqohar/hagglex/ui/fragments/VerifyAccountFragment.kt
@@ -1,10 +1,10 @@
 package com.abdulqohar.hagglex.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
